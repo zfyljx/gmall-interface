60f13037ed7dd2e62179068d181eee4c1774b71e
hxq <2305461452@qq.com>
2019-05-19 14:27:21 +0800

    Initial commit

---
+++ src/main/java/com/hxq/gmall/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.hxq.gmall.service;
+
+import com.hxq.gmall.bean.UserAddress;
+
+import java.util.List;
+
+public interface OrderService {
+	
+	/**
+	 * 初始化订单
+	 * @param userId
+	 */
+	public List<UserAddress> initOrder(String userId);
+
+}
